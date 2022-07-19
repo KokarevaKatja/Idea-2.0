89f3dc8508d66a58033827c7c43ec1d18a0cd013
Екатерина Кокарева <kokarevakatja@gmail.com>
2022-07-19 18:29:17 +0300

    Idea 2.0 changed

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 1000;
+        int one = 20;
+        int total = amount / one;
+
+        System.out.println(total);
+
+    }
+}
